60ffa8c1fa6085d1d41b48998ebc8f9a49e491b0
Butchi Peddi <58072398+butchi-conviva@users.noreply.github.com>
2021-02-17 16:11:45 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ConvivaSDK",
-    exclude: ["ConvivaSDK.podspec", "ConvivaSDK.xcframework", "Framework","README.md"],
+    
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
