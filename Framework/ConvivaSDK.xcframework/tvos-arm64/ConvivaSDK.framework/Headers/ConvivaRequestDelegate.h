f1e5d60a2422749ffe175dde5f21ab2dc4169371
bpeddi <bpeddi@conviva.com>
2021-02-17 16:14:20 +0530

    Removed

--- Framework/ConvivaSDK.xcframework/tvos-arm64/ConvivaSDK.framework/Headers/ConvivaRequestDelegate.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-
-@protocol ConvivaRequestDelegate <NSObject>
-
-- (void)handleRequest:(NSURLRequest *)request
-    completionHandler:(void(^)(NSData *responseData, NSURLResponse *response, NSError *error))completionHandler;
-
-@end
